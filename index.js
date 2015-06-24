bd7dabebb1b543fd7ee71844b2ae2c4c733e420c
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-24 20:02:57 +0530

    Complete basic functionality of the module
    - Prints without any leading labels
    - prints a label with the percentage at the end.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 'use strict';
 module.exports = function (data_array, str, opts) {
 
-	console.log("Main function!");
+	var barChar = require('figures').square;
+	var Stats = require('fast-stats').Stats;
+	var width = require('window-size').width;
+	var maxBarWidth = width - 20;
+	var total_sum = 0.;
+	var percentage = [];
+	var barWidths = [];
+
+	for(var i=0; i<data_array.length; ++i){
+		total_sum += data_array[i];
+	}
+
+	for(var i=0; i<data_array.length; ++i){
+		percentage.push((data_array[i] / total_sum) * 100);
+	}
+
+	var max_percentage = (new Stats().push(percentage)).range()[1];
+
+	for(var i=0; i<percentage.length; ++i){
+		barWidths.push(Math.ceil(percentage[i] / max_percentage * maxBarWidth))
+	}
+
+	for(var i=0; i<percentage.length; ++i){
+		console.log(': ' + new Array(barWidths[i]).join(barChar) + ' ' + percentage[i].toFixed() + '%');
+	}
 
 };
