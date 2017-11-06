9fcfe5a23b1ff1d061130a4873ba800488ae942b
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-11-06 23:11:45 +0530

    make testing output easier to read

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 /* global it */
 'use strict'
-var assert = require('assert')
-var horizontal = require('./')
+var assert = require('assert');
+var horizontal = require('./');
+
+var line = function () {
+  console.log();
+  console.log()
+}
 
 var input_obj = {
 	'first task': 20,
@@ -9,11 +14,33 @@ var input_obj = {
 	'a really really long label!': 90,
 	'short one': 20
 };
+
+line();
 console.log("Bar with labels and values");
 horizontal(input_obj, {labels: true, values: true});
+
+line();
 console.log("Bar with labels and without values, i.e with percentages");
 horizontal(input_obj, {labels: true, values: false});
+
+line();
 console.log("Bar without labels and without values, i.e with percentages");
 horizontal(input_obj, {labels: false, values: false});
+
+line();
 console.log("Default bar graph (must be same as above)");
 horizontal(input_obj);
+
+line();
+console.log("Bar with labels and values (ASCII)");
+horizontal(input_obj, {labels: true, values: true, ascii: true});
+
+line();
+console.log("Bar with labels and without values, i.e with percentages (ASCII)");
+horizontal(input_obj, {labels: true, values: false, ascii: true});
+
+line();
+console.log("Bar without labels and without values, i.e with percentages (ASCII)");
+horizontal(input_obj, {labels: false, values: false, ascii: true});
+
+process.exit(0);
