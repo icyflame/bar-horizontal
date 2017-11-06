3df9928ca7767ba2c4e20587200cc38b5b7938b6
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-11-06 10:21:32 +0530

    add todo notes for code improvements

--- index.js
+++ index.js
@@ -12,22 +12,25 @@ module.exports = function (input_obj, opts) {
   var index
   var values = ObjectValues(input_obj)
 
-	var printValues = false;
+  var printValues = false;
 
-	if (opts !== undefined) {
-		if (opts.values === 'true' || opts.values === true) {
-			printValues = true;
-		}
-	}
+  if (opts !== undefined) {
+	  if (opts.values === 'true' || opts.values === true) {
+		  printValues = true;
+	  }
+  }
 
+  // TODO: Oneliner
   for (index in input_obj) {
     total_sum += input_obj[index]
   }
 
+  // TODO: Use map
   for (index in input_obj) {
     percentage.push((input_obj[index] / total_sum) * 100)
   }
 
+  // TODO: Find maximum directly using something like lodash.max
   var max_percentage = (new Stats().push(percentage)).range()[1]
 
   var fixedLabels = []
@@ -35,11 +38,13 @@ module.exports = function (input_obj, opts) {
   var maxLabelLength = 0
 
   if (labelsTrue) {
+    // TODO: Use oneliners
     for (index in input_obj) {
       maxLabelLength = (index.toString().length > maxLabelLength)
         ? index.toString().length : maxLabelLength
     }
 
+    // TODO: Use oneliners
     for (index in input_obj) {
       var padLength = maxLabelLength - index.toString().length + 1
       fixedLabels.push(index + (padLength > 0
@@ -49,17 +54,22 @@ module.exports = function (input_obj, opts) {
 
   maxBarWidth = labelsTrue ? (maxBarWidth - maxLabelLength) : maxBarWidth
 
+  // TODO: map
   percentage.forEach(function (element, index, array) {
     var potentialWidth = Math.ceil(element / max_percentage * maxBarWidth)
-    barWidths.push(potentialWidth < 0 ? 0 : potentialWidth)
+    barWidths.push(potentialWidth <= 1 ? 0 : potentialWidth)
   })
 
+  console.log(barWidths);
+
+  // TODO: map
   percentage.forEach(function (element, index, array) {
     console.log((labelsTrue ? fixedLabels[index] : '') + ' : ' +
       new Array(barWidths[index]).join(barChar) + '  ' +
       (printValues ? (values[index].toFixed(2)) : (element.toFixed(2) + "%")))
   })
 
+  // TODO: filter
   for (index in barWidths) {
     if (barWidths[index] === 0) {
       if (opts.warnings) {
