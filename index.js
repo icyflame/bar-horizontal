11fa07051de840d8fe0e00218fd4b5b1d1f2a47c
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-11-06 22:58:33 +0530

    improve code, use lodash, remove other dependencies

--- index.js
+++ index.js
@@ -1,78 +1,65 @@
-'use strict'
-module.exports = function (input_obj, opts) {
+'use strict';
+var _ = require('lodash');
+
+module.exports = function (inputObj, opts) {
   // Ensure that `opts` is always an object.
-  opts = (opts && typeof opts === 'object') ? opts : {}
+  opts = (opts && typeof opts === 'object') ? opts : {};
 
-  var barChar = (opts.ascii === true || opts.ascii === 'true') ? '=' : require('figures').square
-  var Stats = require('fast-stats').Stats
-  var width = require('window-size').width
-  var ObjectValues = require('object.values')
+  var width = require('window-size').width;
+  var barChar = opts.ascii ? '=' : require('figures').square;
 
-  var maxBarWidth = width - (width > 20 ? 20 : 1)
-  var total_sum = 0.0
-  var percentage = []
-  var barWidths = []
-  var index
-  var values = ObjectValues(input_obj)
-  var printValues = (opts.values === 'true' || opts.values === true);
+  var maxBarWidth = width - (width > 20 ? 20 : 1);
 
-  // TODO: Oneliner
-  for (index in input_obj) {
-    total_sum += input_obj[index]
-  }
+  var totalSum = 0.0;
+  var percentage = [];
+  var barWidths = [];
 
-  // TODO: Use map
-  for (index in input_obj) {
-    percentage.push((input_obj[index] / total_sum) * 100)
-  }
+  var values = _.values(inputObj);
+  var keys = _.keys(inputObj);
 
-  // TODO: Find maximum directly using something like lodash.max
-  var max_percentage = (new Stats().push(percentage)).range()[1]
+  var printValues = opts.values;
 
-  var fixedLabels = []
-  var labelsTrue = opts ? opts.labels : false
-  var maxLabelLength = 0
+  totalSum = _.sum(inputObj);
+  percentage = _.map(inputObj, (elem) => elem / totalSum * 100);
+  var maxPercentage = _.max(percentage);
+
+  var fixedLabels = [];
+
+  var labelsTrue = opts.labels || false;
+
+  var maxLabelLength = 0;
 
   if (labelsTrue) {
-    // TODO: Use oneliners
-    for (index in input_obj) {
-      maxLabelLength = (index.toString().length > maxLabelLength)
-        ? index.toString().length : maxLabelLength
-    }
-
-    // TODO: Use oneliners
-    for (index in input_obj) {
-      var padLength = maxLabelLength - index.toString().length + 1
-      fixedLabels.push(index + (padLength > 0
-          ? new Array(padLength).join(' ') : ''))
-    }
+    maxLabelLength = _.max(_.map(keys, e => e.toString().length));
+    fixedLabels = _.map(inputObj, (value, key) => {
+      var padLength = maxLabelLength - key.toString().length + 1;
+      return padLength > 0 ? (new Array(padLength).join(' ')) : '';
+    });
   }
 
-  maxBarWidth = labelsTrue ? (maxBarWidth - maxLabelLength) : maxBarWidth
+  maxBarWidth = labelsTrue ? (maxBarWidth - maxLabelLength) : maxBarWidth;
 
-  // TODO: map
+  barWidths = _.map(percentage, elem => {
+    var potentialWidth = Math.ceil(elem / maxPercentage * maxBarWidth);
+    return (potentialWidth <= 1 ? 0 : potentialWidth);
+  });
+
   percentage.forEach(function (element, index, array) {
-    var potentialWidth = Math.ceil(element / max_percentage * maxBarWidth)
-    barWidths.push(potentialWidth <= 1 ? 0 : potentialWidth)
-  })
+    var elementLine = (labelsTrue ? fixedLabels[index] : '');
+    elementLine += ' : ';
+    elementLine += new Array(barWidths[index]).join(barChar);
+    elementLine += '  ';
+    elementLine += (printValues ? (values[index].toFixed(2)) : (element.toFixed(2) + '%'));
+    console.log(elementLine);
+  });
 
-  console.log(barWidths);
+  if (_.filter(values, 0).length > 0 && opts.warnings) {
+    var message = "This terminal's width is too less!";
 
-  // TODO: map
-  percentage.forEach(function (element, index, array) {
-    console.log((labelsTrue ? fixedLabels[index] : '') + ' : ' +
-      new Array(barWidths[index]).join(barChar) + '  ' +
-      (printValues ? (values[index].toFixed(2)) : (element.toFixed(2) + "%")))
-  })
-
-  // TODO: filter
-  for (index in barWidths) {
-    if (barWidths[index] === 0) {
-      if (opts.warnings) {
-        console.log("This terminal's width is too less!" + (labelsTrue ? ' You can remove labels, and try again!' : ''))
-      }
-      break
+    if (labelsTrue) {
+      message += ' You can remove labels, and try again!';
     }
-  }
 
-}
+    console.log(message);
+  }
+};
