af24daa80e7e61c4ec8caf700338ec4b1783d9fa
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-11-13 18:25:24 -0500

    Stub out new tests in 'test/bar-horizontal.spec.js'. Update 'test' script.

---
+++ test/bar-horizontal.spec.js
@@ -0,0 +1,63 @@
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+// Node
+const assert = require( 'assert' );
+
+// Project
+const barHorizontal = require( '../' );
+
+// --------------------------------------------------
+// DECLARE TESTS
+// --------------------------------------------------
+describe( 'barHorizontal', function() {
+  // Ensure that module correctly exports a function (eg. module is 'require-able').
+  xit( 'Should export a function.', function() {
+
+  } );
+
+  // Ensure that function correctly exits when invoked with a missing/invalid inputObj.
+  xit( 'Should exit when invoked with a missing/invalid `inputObj`.', function() {
+
+  } );
+
+  // Ensure that function can be invoked with 1x argument (eg. inputObj).
+  xit( 'Should run as normal when invoked with a single, valid argument.', function() {
+
+  } );
+
+  // Ensure that function can be invoked with 2x arguments (eg. inputObj, options).
+  xit( 'Should accept an `options` object as an optional second argument.', function() {
+
+  } );
+
+  // Ensure that 'labels' are NOT displayed by default.
+  xit( 'Should not display labels by default.', function() {
+
+  } );
+
+  // Ensure that 'labels' are displayed when options contains the labels: true key/value pair.
+  xit( 'Should display labels when invoked with the `labels: true` key/value pair.', function() {
+
+  } );
+
+  // Ensure that 'warnings' are NOT displayed by default.
+  xit( 'Should not display warnings by default.', function() {
+
+  } );
+
+  // Ensure that 'warnings' are displayed when options contains the warnings: true key/value pair.
+  xit( 'Should display warnings when invoked with the `warnings: true` key/value pair.', function() {
+
+  } );
+
+  // Ensure that the bar graphs are composed of the square character by default.
+  xit( 'Should create bar graphs using the `square` character by default.', function() {
+
+  } );
+
+  // Ensure that the bar graphs are composed of the '=' character when options contains the ascii: true key/value pair.
+  xit( 'Should create bar graphs using the "=" character when invoked with the `ascii: true` key/value pair.', function() {
+
+  } );
+} );
